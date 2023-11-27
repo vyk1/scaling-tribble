a5ab664aadc7f2e9114598d6e8da1282a6122bef
Victoria <47799125+vyk1@users.noreply.github.com>
2023-11-27 16:13:50 -0300

    Update extract-userTasks-id-name-camunda:formKey.sh

--- extract-userTasks-id-name-camunda:formKey.sh
+++ extract-userTasks-id-name-camunda:formKey.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat src/main/resources/p-solicitar-materiais.bpmn | grep -oP "(?<=userTask ).*.html" | sort | uniq > forms-com-id.txt
+cat src/main/resources/p-process.bpmn | grep -oP "(?<=userTask ).*.html" | sort | uniq > forms-com-id.txt
